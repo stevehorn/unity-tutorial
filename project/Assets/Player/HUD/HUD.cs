19586ca660d171c6ea00a1c50c9655aaf51447d7
Steve Horn <steve@stevehorn.cc>
2013-11-15 22:50:38 -0500

    debugging click

--- project/Assets/Player/HUD/HUD.cs
+++ project/Assets/Player/HUD/HUD.cs
@@ -6,6 +6,7 @@ public class HUD : MonoBehaviour {
 	private Player player;
 	public GUISkin resourceSkin, ordersSkin;
 	private const int ORDERS_BAR_WIDTH = 150, RESOURCE_BAR_HEIGHT = 40;
+	private const int SELECTION_NAME_HEIGHT = 15;
 
 	// Use this for initialization
 	void Start () {
@@ -19,12 +20,29 @@ public class HUD : MonoBehaviour {
 		    DrawResourceBar();
 		}
 	}
+
+	public bool MouseInBounds() {
+		//Screen coordinates start in the lower-left corner of the screen
+		//not the top-right of the screen like the drawing coordinates do
+		Vector3 mousePos = Input.mousePosition;
+		bool insideWidth = mousePos.x >= 0 && mousePos.x <= Screen.width - ORDERS_BAR_WIDTH;      
+		bool insideHeight = mousePos.y >= 0 && mousePos.y <= Screen.height - RESOURCE_BAR_HEIGHT;
+		return insideWidth && insideHeight;
+	}
 	
 	private void DrawOrdersBar() {
 	    GUI.skin = ordersSkin;
 	    GUI.BeginGroup(new Rect(Screen.width-ORDERS_BAR_WIDTH,RESOURCE_BAR_HEIGHT,ORDERS_BAR_WIDTH,Screen.height-RESOURCE_BAR_HEIGHT));
 	    GUI.Box(new Rect(0,0,ORDERS_BAR_WIDTH,Screen.height-RESOURCE_BAR_HEIGHT),"");
 	    GUI.EndGroup();
+
+		string selectionName = "";
+		if(player.SelectedObject) {
+			selectionName = player.SelectedObject.objectName;
+		}
+		if(!selectionName.Equals("")) {
+			GUI.Label(new Rect(0,10,ORDERS_BAR_WIDTH,SELECTION_NAME_HEIGHT), selectionName);
+		}
 	}
 	
 	private void DrawResourceBar() {
--- project/Assets/Player/UserInput.cs
+++ project/Assets/Player/UserInput.cs
@@ -16,13 +16,44 @@ public class UserInput : MonoBehaviour {
         if(player.human) {
             MoveCamera();
             RotateCamera();
+			MouseActivity();
         }
     }
- 
-    private void MoveCamera() {
-        float xpos = Input.mousePosition.x;
-        float ypos = Input.mousePosition.y;
-        Vector3 movement = new Vector3(0,0,0);
+
+	private void MouseActivity() {
+		if(Input.GetMouseButtonDown(0)) LeftMouseClick();
+		else if(Input.GetMouseButtonDown(1)) RightMouseClick();
+	}
+
+	private void LeftMouseClick() {
+		if(player.hud.MouseInBounds()) {
+			GameObject hitObject = FindHitObject();
+			Vector3 hitPoint = FindHitPoint();
+			if(hitObject && hitPoint != ResourceManager.InvalidPosition) {
+				if(player.SelectedObject) player.SelectedObject.MouseClick(hitObject, hitPoint, player);
+				else if(hitObject.name!="Ground") {
+					WorldObject worldObject = hitObject.transform.root.GetComponent<WorldObject>();
+					if(worldObject) {
+						//we already know the player has no selected object
+						player.SelectedObject = worldObject;
+						worldObject.SetSelection(true);
+					}
+				}
+			}
+		}
+	}
+
+	private void RightMouseClick() {
+		if(player.hud.MouseInBounds() && !Input.GetKey(KeyCode.LeftAlt) && player.SelectedObject) {
+			player.SelectedObject.SetSelection(false);
+			player.SelectedObject = null;
+		}
+	}
+	
+	private void MoveCamera() {
+		float xpos = Input.mousePosition.x;
+		float ypos = Input.mousePosition.y;
+		Vector3 movement = new Vector3(0,0,0);
  
         //horizontal camera movement
         if(xpos >= 0 && xpos < ResourceManager.ScrollWidth) {
@@ -81,4 +112,18 @@ public class UserInput : MonoBehaviour {
             Camera.mainCamera.transform.eulerAngles = Vector3.MoveTowards(origin, destination, Time.deltaTime * ResourceManager.RotateSpeed);
         }
     }
+
+	private Vector3 FindHitPoint() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit)) return hit.point;
+		return ResourceManager.InvalidPosition;
+	}
+	
+	private GameObject FindHitObject() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit)) return hit.collider.gameObject;
+		return null;
+	}
 }
